4d708f69e975fc36b7e4e68e9c53e6dd81f56214
LiageSTD <368952@edu.itmo.ru>
2025-02-06 21:54:20 +0300

    PetCoin: CurrencyUpdater stub added

--- src/main/java/org/coinpet/petcoin/crypto/service/currencyUpdater/CurrencyUpdaterService.java
+++ src/main/java/org/coinpet/petcoin/crypto/service/currencyUpdater/CurrencyUpdaterService.java
@@ -1,4 +1,13 @@
 package org.coinpet.petcoin.crypto.service.currencyUpdater;
 
 public class CurrencyUpdaterService implements CurrencyUpdater {
+    @Override
+    public void updateCurrency(String currency) {
+
+    }
+
+    @Override
+    public void updateAllAvailableCurrencies() {
+
+    }
 }
